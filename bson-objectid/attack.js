d9c16903a36f409adacf43bda7f5c02beeeb101a
nayanadasgupta <nayana.dasgupta@outlook.com>
2022-03-06 10:53:56 +0000

    Update bson-objectid with new fake id

--- bson-objectid/attack.js
+++ bson-objectid/attack.js
@@ -6,13 +6,17 @@ console.log(ObjectID.isValid(ObjectID("54495ad94c934721ede76d90")));
 
 // Attack Example
 const payload = {
-    "id": "KILLL",
+    "id": "BAD_ID",
     "hello": "world",
     "_bsontype" : "ObjectID"
 };
 
-console.log("here", ObjectID("54495ad94c934721ede76d90").equals(ObjectID(payload)))
-console.log(ObjectID("54495ad94c934721ede76d90").id)
+// console.log("here", ObjectID("54495ad94c934721ede76d90").equals(ObjectID(payload)))
+
+
+// console.log(ObjectID("54495ad94c934721ede76d90").id)
+
+
 console.log(ObjectID(payload).id)
 console.log(ObjectID(payload));
 console.log(ObjectID.isValid(ObjectID(payload)));
