47c98a7a2d83be93ad8a7af86a370cf8e05d81ba
Warren Buhler <warrenbuhler7@gmail.com>
2022-03-10 10:06:00 +0000

    minor changes

--- jpv/2.2.1_JPV_internal_attack.js
+++ jpv/2.2.1_JPV_internal_attack.js
@@ -9,7 +9,8 @@ function exampleJPV() {
 	const schema = {
 		aMap: new Map()
 	};
-
+	console.log(typeof (schema));
+	
 	console.log("This bad input below tricks jpv");
 
 	console.log("	aMap: {\n	badcode: \"problematic input.\",\n	constructor: new Map().constructor \n	}");
@@ -20,8 +21,30 @@ function exampleJPV() {
 		  constructor: new Map.constructor
 		}
 	};
-
+	
 	// jpv.validate(input, schema) should return false, but, as of 2.2.1, returns true
 	console.log("jpv.validate(input, schema) = " + jpv.validate(input, schema));
+	console.log(input.constructor === schema.constructor);
+
+	for (const property in input.aMap) {
+		console.log(property);
+		console.log("i am here");
+		if (Object.prototype.hasOwnProperty.call(input.aMap, String(property))) {
+			console.log("now here");
+			console.log(property);
+			
+			console.log(Object.prototype.hasOwnProperty.call(input.aMap, "constructor"));
+			console.log(Object.prototype.hasOwnProperty.call(schema.aMap, "constructor"))
+
+			if (!Object.prototype.hasOwnProperty.call(schema, String(property))) {
+				console.log("checked schema");
+            }
+			//if ((typeof input[property] === 'object') &&
+			//	(typeof schema[property] === 'object') &&
+			//	(Object.keys(schema[property]).length !== 0)) {
+			//	return (valid = iterate(value[property], pattern[property], valid, cb, options));
+			//}
+		}
+	}
 
 }
