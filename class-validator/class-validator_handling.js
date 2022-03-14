2514e8723ac28b281533ea116a93cda62ca84309
Warren Buhler <warrenbuhler7@gmail.com>
2022-03-14 22:12:03 +0000

    fixing final sql request needs

--- class-validator/class-validator_handling.js
+++ class-validator/class-validator_handling.js
@@ -4,9 +4,50 @@ import { UserSchema } from "./schema.js";
 import { validate } from "class-validator";
 import { createConnection } from 'mysql2';
 
-function login(emailInput, passwordInput, connection)
-{
-    const sqlquery1 = `SELECT * FROM login WHERE email = ${emailInput} AND password = ${passwordInput}`;
+// Handle the user sent over via JSON
+function jsonHandle(emailInput) {
+    let requirements = {
+        host: 'localhost',
+        user: 'root',
+        password: 'compsec2',
+        database: 'sqldatabase'
+    };
+    var connection = createConnection(requirements);
+    console.log(emailInput);
+    console.log(UserSchema);
+
+    let schema = new UserSchema();
+    schema.email = '';
+    schema.password = '';
+
+    let test1Param = Object.assign(schema, emailInput);
+    console.log("This is the merged schema:")
+    console.log(test1Param);
+
+    return new Promise(function (resolve, reject) {
+        validate(test1Param).then((errors) => {
+            if (errors.length > 0) {
+                console.log('invalid email and or password, unable to validate user', errors);
+                resolve("Class validator failed to validate user ");
+            } else {
+                console.log('valid email and password, user successfully validated. Relevant Database Information:');
+                login(test1Param.email, test1Param.password, connection).then((results) => {
+
+                    if (results.length == 0) {
+                        resolve("No user found");
+
+                    }
+                    resolve(results);
+                })
+            }
+            connection.end();
+        });
+    });
+}
+
+// Login function for inputs that passed validation
+function login(emailInput, passwordInput, connection) {
+    const sqlquery1 = `SELECT * FROM login WHERE email = '${emailInput}' AND password = '${passwordInput}'`;
     console.log(sqlquery1);
     return new Promise(function (resolve, reject) {
         connection.query(sqlquery1, function (error, rows) {
@@ -22,39 +63,6 @@ function login(emailInput, passwordInput, connection)
 
 }
 
-
-function jsonHandle(emailInput) {
-    let requirements = {
-        host: 'localhost',
-        user: 'root',
-        password: 'compsec2',
-        database: 'sqldatabase'
-    };
-    var connection = createConnection(requirements);
-
-    let test1Param = Object.assign(UserSchema, emailInput);
-    console.log("This is the merged schema:")
-    console.log(test1Param);
-
-    return new Promise(function (resolve, reject) {
-        validate(test1Param).then((errors) => {
-            if (errors.length > 0) {
-                console.log('invalid email and or password, unable to validate user', errors);
-                resolve("Class validator failed to validate user ");
-            } else {
-                console.log('valid email and password, user successfully validated. Relevant Database Information:');
-                login(test1Param.email, test1Param.password, connection).then((results) => {
-
-                    if (results.length == 0) {
-                        resolve("No user found");
-                    }
-                    resolve(results);
-                })
-            }
-        });
-    });
-
-}
 
 export default {
     jsonHandle
--- class-validator/schema.js
+++ class-validator/schema.js
@@ -4,7 +4,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { IsEmail, MinLength, MaxLength } from 'class-validator';
+import { IsEmail, MinLength, MaxLength, IsDefined } from 'class-validator';
 export class UserSchema {
 }
 __decorate([
@@ -12,5 +12,6 @@ __decorate([
 ], UserSchema.prototype, "email", void 0);
 __decorate([
     MinLength(5),
-    MaxLength(15)
+    MaxLength(15),
+    IsDefined()
 ], UserSchema.prototype, "password", void 0);
